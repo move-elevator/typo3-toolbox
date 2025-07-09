e865e310b5b53a1215deee84f0e07d0584f1ef05
Ronny Hauptvogel <rh@move-elevator.de>
2025-07-09 12:22:47 +0200

    build: update rector config

--- Classes/EventListener/SaveCloseButtonEventListener.php
+++ Classes/EventListener/SaveCloseButtonEventListener.php
@@ -8,8 +8,8 @@ use TYPO3\CMS\Backend\Template\Components\ButtonBar;
 use TYPO3\CMS\Backend\Template\Components\Buttons\InputButton;
 use TYPO3\CMS\Backend\Template\Components\ModifyButtonBarEvent;
 use TYPO3\CMS\Core\Attribute\AsEventListener;
-use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Imaging\IconSize;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Page\PageRenderer;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -38,7 +38,7 @@ final class SaveCloseButtonEventListener
                     'js' => 'save-and-close-button',
                 ])
                 ->setTitle($title)
-                ->setIcon($iconFactory->getIcon('actions-document-save-close', Icon::SIZE_SMALL))
+                ->setIcon($iconFactory->getIcon('actions-document-save-close', IconSize::SMALL))
                 ->setShowLabelText(true);
 
             $buttons[ButtonBar::BUTTON_POSITION_LEFT][2][] = $saveCloseButton;
